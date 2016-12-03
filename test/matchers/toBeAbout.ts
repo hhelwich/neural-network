8935dbbf5cbed240acc78f26cbb8e106070c92d9
Hendrik Helwich <hendrik@helwich.de>
2016-12-04 00:16:47 +0100

    Make toBeAbout() matcher message more informative

--- test/matchers/toBeAbout.ts
+++ test/matchers/toBeAbout.ts
@@ -1,34 +1,25 @@
-const { abs } = Math;
+const { abs, max } = Math;
 
 /** If given input is a number, wrap it in a list. Otherwise identity */
 const assureNbrList = (a: number|number[]) => typeof a === 'number' ? [a] : a;
 
-/** Returns true if the difference between the given numbers is lower or equal to maxDifference */
-const isAboutNbr = (a: number, b: number, maxDifference: number) => abs(a - b) <= maxDifference;
-
-/**
- * Returns true if the differences of all numbers with the same indices in the given lists are lower or equal than
- * maxDifference
- */
-const isAbout = (a: number[]|number, b: number[]|number, maxDifference: number) => {
+/** Returns the maximum difference for all number pairs */
+const maxDiff = (a: number[]|number, b: number[]|number) => {
     a = assureNbrList(a);
     b = assureNbrList(b);
     if (a.length !== b.length) {
-        return false;
+        throw Error('Compared lists must have the same length');
     }
-    for (let i = 0, len = a.length; i < len; i++) {
-        if (!isAboutNbr(a[i], b[i], maxDifference)) {
-            return false;
-        }
-    }
-    return true;
+    const diff = a.map((value, i) => abs(value - b[i]));
+    return max(0, ...diff);
 };
 
 export const toBeAbout: jasmine.CustomMatcherFactory = (util, customEqualityTesters) => {
     return {
         compare: (actual: number|number[], expected: number|number[], maxDifference: number = 0) => {
-            const pass = isAbout(actual, expected, maxDifference);
-            const message = 'Expected difference to be ' + (pass ? '> ' : '<= ') + maxDifference;
+            const diff = maxDiff(actual, expected);
+            const pass = diff <= maxDifference;
+            const message = 'Expected max difference to be ' + (pass ? '> ' : '<= ') + maxDifference + ' but was ' + diff;
             return { pass, message };
         }
     };
